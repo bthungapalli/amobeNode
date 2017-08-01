5d3376192cea6aecbdfb1bbc17304e5566df5554
apatha <apatha@NIST-MAC-L-0051s-MacBook-Pro.local>
2017-08-01 16:42:41 +0530

    [Alok]: file

--- public/angularJs/modules/dashboard/controllers/problem.controller.js
+++ public/angularJs/modules/dashboard/controllers/problem.controller.js
@@ -11,7 +11,7 @@
 			    inline: false,
 			    plugins: [
 		                    'advlist autolink lists link image charmap print preview anchor',
-		                    'searchreplace visualblocks code fullscreen',
+		                    'searchreplace visualblocks code',
 		                    'insertdatetime media table contextmenu paste code'
 		                ],
 		        toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
